cdcd4dbd43dad32a84588c803d33b89618a4205e
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 14:59:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ end
 def reduce(source_arr, start_value = 0)
   new_value = start_value
   source_arr.length.times { |index|
-    if new_value == Integer ? new_value = yield(source_arr[index], new_value) : new_value = !!yield(source_arr[index], new_value)
+    if new_value.is_a(Integer) ? new_value = yield(source_arr[index], new_value) : new_value = !!yield(source_arr[index], new_value)
     end
   }
   p new_value
