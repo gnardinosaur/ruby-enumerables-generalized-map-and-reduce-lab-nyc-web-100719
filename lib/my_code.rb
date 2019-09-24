22d44fe95761a3844fb164ee7c43c3abd51dcce3
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 13:43:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++
@@ -1,5 +0,0 @@
-def make_sandwich(element1, element2)
-  yield("A #{element1} and #{element2} sandwich")
-end
-
-p make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
