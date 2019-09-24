c7415ec990a22984540181a8a387c109ac845318
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 14:54:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,7 +6,7 @@ def map(source_arr)
   new_arr
 end 
 
-def reduce(source_arr, start_value = "")
+def reduce(source_arr, start_value = 0)
   new_value = start_value
   source_arr.length.times { |index|
     new_value = yield(source_arr[index], new_value)
