ce5a11ca4832a47df4b048d3b72a6db1e9a3e4f5
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 14:39:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,9 +11,8 @@ def reduce(source_arr, start_value = 0)
   source_arr.length.times { |index|
     new_value = yield(source_arr[index], new_value)
   }
-  if new_value = Integer
-    return new_value
-  else
+  if !new_value = Integer
     return !!new_value
   end
+  new_value
 end 
